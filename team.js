ba442663ee72cce61e321d1effff4be3ab02a386
MfrMesa <152450508+MfrMesa@users.noreply.github.com>
2023-12-07 23:12:22 +0100

    Cambio de orden

---
+++ team.js
@@ -0,0 +1,12 @@
+let students= [
+    {name: "Maria Fernanda", 
+    surname: "Gomez", 
+    age: 30, 
+    city: "Madrid", 
+    hobby: "Paint", 
+    favoriteFood: "Sushi", 
+    favoriteVideoGame: "WOW", 
+    favoriteFilm: "Hicth", 
+    favoriteBook: "The Kingkiller Chronicles", 
+    petName: "Alhue"},
+]
