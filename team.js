6e155d0fa33b6d64a9c00cb61f0b24dd29282915
EMILIOFC <julitocazalla96@gmail.com>
2023-12-07 14:39:22 +0100

    añadido datos

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let students= []
+let students= [
+    {
+    name: "Emilio Fernandez Carmona",
+    surname: "Fernandez Carmona",
+    age:27,
+    city: "Sevilla",
+    hobby:"programar",
+    favoriteFood:"manzana",
+    favoriteVideoGame:"red redetion",
+    favoriteFilm:"Señor de los Anillos",
+    favoriteBook:"Señor de los Anillos",
+    petName:"Noa",
+    }
+]
