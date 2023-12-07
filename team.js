0e9b420de2c93dff4f8e767e9447ce3eadd629bd
MfrMesa <152450508+MfrMesa@users.noreply.github.com>
2023-12-07 15:01:10 +0100

    Cambios de Fer

--- team.js
+++ team.js
@@ -1,1 +1,3 @@
-let students= []
+let students= [
+    {surname: "Fernanda", age: 30, city: "Madrid", hobby: "Paint", favoriteFood: "Sushi", favoriteVideoGame: "WOW", favoriteFilm: "Hicth", favoriteBook: "The Kingkiller Chronicles", petName: "Alhue"},
+]
