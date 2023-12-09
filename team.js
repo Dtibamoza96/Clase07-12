e37edb0b378fdde0c6e528436266f7223d0b3d8b
Di  Mark <alvaro.donoso.marquez@gmail.com>
2023-12-09 16:27:51 +0100

    Update 1

--- team.js
+++ team.js
@@ -1,1 +1,11 @@
-let students= []
+let students= [
+    nombre= "Alvaro Donoso Marquez",
+    age= 27,
+    city="Ciudad real",
+    hobby="Videojuegos",
+    favoriteFood="Saitan en salsa",
+    favoriteVideoGame="Star Citizen",
+    favoriteFilm="El atlas de las nubes",
+    favoriteBook="El extranjero, Albert Camus",
+    petName="Carry Fishering Money"
+   ]
