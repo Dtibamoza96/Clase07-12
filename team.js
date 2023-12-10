7670872ad3a50c4f5f641a89e71089f0cd2bdb87
MfrMesa <152450508+MfrMesa@users.noreply.github.com>
2023-12-10 12:25:22 +0100

    modified:   team.js

--- team.js
+++ team.js
@@ -1,12 +1,3 @@
-let students= [
-    {name: "Maria Fernanda", 
-    surname: "Gomez", 
-    age: 30, 
-    city: "Madrid", 
-    hobby: "Paint", 
-    favoriteFood: "Sushi", 
-    favoriteVideoGame: "WOW", 
-    favoriteFilm: "Hicth", 
-    favoriteBook: "The Kingkiller Chronicles", 
-    petName: "Alhue"},
+let students = [
+    
 ]
