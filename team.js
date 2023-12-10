4f0bc9c7a3684367d070d2ec45e96b75d11a11ad
MfrMesa <152450508+MfrMesa@users.noreply.github.com>
2023-12-10 13:06:24 +0100

    merged

--- team.js
+++ team.js
@@ -1,4 +1,4 @@
-let students = [
+let students= [
     {name: "Maria Fernanda", 
     surname: "Gomez", 
     age: 30, 
@@ -10,14 +10,16 @@ let students = [
     favoriteBook: "The Kingkiller Chronicles", 
     petName: "Alhue"},
 
-    { name: "Imad",
-    surname: "El Founti Chaib",
-    age: 23,
-    city: "Málaga",
-    hobby: "Programador",
-    favoriteFood: "Pinchos",
-    favoriteVideoGame: "Marvel's Spiderman",
-    favoriteFilm: "Imitation Game",
-    favoriteBook: "El Principito",
-    petName: "Lina"},
-    ]
+    {
+        name: "Imad",
+        surname: "El Founti Chaib",
+        age: 23,
+        city: "Málaga",
+        hobby: "Programador",
+        favoriteFood: "Pinchos",
+        favoriteVideoGame: "Marvel's Spiderman",
+        favoriteFilm: "Imitation Game",
+        favoriteBook: "El Principito",
+        petName: "Lina"
+    },
+]
