aee51edcc4e5c65b3b0f2cb43a773c2386cb2760
Virtualchess <rafalejandromedina@gmail.com>
2023-12-12 15:33:52 +0100

    estructura mejor

--- team.js
+++ team.js
@@ -1,2 +1,14 @@
-let students= [{name: "Rafael Medina",age:23,city:"Madrid",hobby:"video games",favoriteFood:"Pokke",favoriteVideoGame:"Dota2",favoriteFilm:"Lord of The Rings",favoriteBook:"El Alquimista",petName:"Lucy"}]
+let students= [
+    {
+        name: "Rafael Medina",
+        age:23,
+        city:"Madrid",
+        hobby:"video games",
+        favoriteFood:"Pokke",
+        favoriteVideoGame:"Dota2",
+        favoriteFilm:"Lord of The Rings",
+        favoriteBook:"El Alquimista",
+        petName:"Lucy"
+    }
+]
 
