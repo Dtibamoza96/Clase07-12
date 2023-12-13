a8ebbb7630e60b63561756ca3d22adbe196bdde6
Virtualchess <rafalejandromedina@gmail.com>
2023-12-13 08:04:27 +0100

    subiendo funcion

--- team.js
+++ team.js
@@ -11,4 +11,18 @@ let students= [
         petName:"Lucy"
     }
 ]
+/*Print who has a pet (name petName).*/
 
+function havePet(studentList) {
+    for (let i = 0; i < studentList.length; i++) {
+        if (studentList[i].petName !== undefined && studentList[i].petName !== null && studentList[i].petName !== "") {
+            console.log(`${studentList[i].name} have a pet called ${studentList[i].petName}.`);
+        } else if (studentList[i].petName === "") {
+            console.log(`${studentList[i].name} please write down the name of your pet.`);
+        } else {
+            console.log(`${studentList[i].name} doesn't have a pet.`);
+        }
+    }
+}
+
+havePet(students);
